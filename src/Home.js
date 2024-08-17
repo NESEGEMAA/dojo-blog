170cb98d3d0bdd379e3d95d9d45b1c90905fff9e
NESEGEMAA <65955410+NESEGEMAA@users.noreply.github.com>
2024-08-17 23:04:07 +0300

    Refactored the code to add the blogs dynamically, added a function to delete blogs as well.

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -7,14 +8,14 @@ const Home = () => {
         { title: 'Web Dev Top Tips', body: 'Lorem Impsum....', author: 'mario', id: 3 }
     ]);
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog) => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
     return (
         <div className="Home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={ blog.id }>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                </div>
-            ))}
+            <BlogList blogs={ blogs } title="All blogs!" handleDelete={ handleDelete } />
         </div>
     );
 }
