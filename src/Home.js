5b17a3c1f110e69df2f93fd6d7efc348b213b1a6
NESEGEMAA <65955410+NESEGEMAA@users.noreply.github.com>
2024-08-18 23:36:53 +0300

    Added a JSON file that would act as a data base for the blogs, and incorprated that in the Home nad BlogList files. Introduced using Hooks and useEffect to fetch data from the new JSON database.

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My New Website', body: 'Lorem Impsum....', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'Lorem Impsum....', author: 'yoshi', id: 2 },
-        { title: 'Web Dev Top Tips', body: 'Lorem Impsum....', author: 'mario', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter((blog) => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then((data) => {
+                setBlogs(data)
+            })
+    }, []);
 
     return (
         <div className="Home">
-            <BlogList blogs={ blogs } title="All blogs!" handleDelete={ handleDelete } />
+            { blogs && <BlogList blogs={ blogs } title="All blogs!" /> }
         </div>
     );
 }
